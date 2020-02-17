127b068791851e3968768bfb13142d56565314f5
Keith Lee <lee.mingkeith@gmail.com>
2020-02-17 21:35:21 +0900

    Initial Commit

---
+++ PrimeNumberFinderOSXApplication/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PrimeNumberFinderOSXApplication
+//
+//  Created by Keith Lee on 2020/02/17.
+//  Copyright © 2020 Keith Lee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PrimeNumberFinderOSXApplication/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PrimeNumberFinderOSXApplication
+//
+//  Created by Keith Lee on 2020/02/17.
+//  Copyright © 2020 Keith Lee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
